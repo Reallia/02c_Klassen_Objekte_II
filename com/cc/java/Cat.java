9a2b82f742cf92c3565c57cd4b3bdc396b8c8db0
BaeckerAnna <baecker.anna2@gmail.com>
2022-07-22 11:06:17 +0200

    OOP und PP return entfernt

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,14 +2,22 @@ package com.cc.java;
 
 public class Cat {
     
+
+
     private String name;
     private String furColor;
     private int age;
+    private boolean isFemale;
+
+    private int counter; // braucht keinen anfangswert, ethält eine default 0
+
+    
    
-    public Cat(String name, String furColor, int age) {
+    public Cat(String name, String furColor, int age, boolean isFemale) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
+        this.isFemale = isFemale;
     }
 
     /** Getter */
@@ -24,10 +32,30 @@ public class Cat {
         }
     }
 
-    public int getAge() {
-        return age;
+    public String getAge() {
+        
+        if (isFemale) {
+            return checkEscalationsLevel();
+        } else {
+            return String.valueOf(age);
+        }
     }
 
+    private String checkEscalationsLevel() {
 
+        counter++;
+
+        switch (counter) {
+            case 1:
+                return "This is an inaproppriate question!";
+            case 2:
+                return "I have told you once";            
+            case 3:
+                return "Talk to the hand!";    
+            default:
+                return "beep";  // durch das return braucht man kein break; mehr 
+                                // return bricht eine methode ab.
+        }
+    }
 }
 
